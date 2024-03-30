1501674bb1de3de95fe0f01f2008bbde9253a727
Tailor <teilorza17@gmail.com>
2024-03-30 10:03:08 -0500

    creacion de los metodos expresion, actualizar, eliminar

--- calculadora.py
+++ calculadora.py
@@ -16,7 +16,9 @@ class Calculadora(QMainWindow):
         self.componente_general.setLayout(self.layout_principal)
         # Metodos para crear la parte visual de la calculadora
         self._crear_area_captura()
-        self._crear_botone()
+        # Agregamos los botones
+        self._crear_botones()
+        # Conectamos las señales con los slots
         self._conectar_botones()
 
     def _crear_area_captura(self):
@@ -28,54 +30,62 @@ class Calculadora(QMainWindow):
         # Agregamos la linea de entrada al layout principal
         self.layout_principal.addWidget(self.linea_entrada)
 
-    def _crear_botone(self):
-        #creamos un diccionario para definir cada boton de la calculadora
-        self.botonoes = {}
+    def _crear_botones(self):
+        # Creamos un diccionario para definir cada boton (texto) de la calculadora
+        self.botones = {}
         layout_botones = QGridLayout()
-        #texto y posicion en el grid layout
+        # Texto | Posicion en el grid layout
         self.botones = {
-            '7': (0, 0),
-            '8': (0, 1),
-            '9': (0, 2),
-            '/': (0, 3),
-            '4': (1, 0),
-            '5': (1, 1),
-            '6': (1, 2),
-            '*': (1, 3),
-            '3': (2, 0),
-            '2': (2, 1),
-            '1': (2, 2),
-            '-': (2, 3),
-            '0': (3, 0),
-            '.': (3, 1),
-            'C': (3, 2),
-            '+': (3, 3),
-            '=': (3, 4)
+            '7':(0,0),
+            '8':(0,1),
+            '9':(0,2),
+            '/':(0,3),
+            '4':(1,0),
+            '5':(1,1),
+            '6':(1,2),
+            '*':(1,3),
+            '1':(2,0),
+            '2':(2,1),
+            '3':(2,2),
+            '-':(2,3),
+            '0':(3,0),
+            '.':(3,1),
+            'C':(3,2),
+            '+':(3,3),
+            '=':(3,4)
         }
-        #creamos los botones y los agregamos al layot
-        #la posicion es una tuple con valores
-        for text_boton, posicion in self.botones.items():
-            self.botonoes[text_boton] = QPushButton(text_boton)
-            self.botonoes[text_boton].setFixedSize(40, 40)
-            #publicar botones
-            layout_botones.addWidget(self.botonoes[text_boton], posicion[0], posicion[1])
-        #agergamos el layout de bootones al principal
+
+        # Creamos los botones y los agregamos al grid layout
+        # La posicion es una tupla con dos valores (renglon-columna)
+        for texto_boton, posicion in self.botones.items():
+            self.botones[texto_boton] = QPushButton(texto_boton)
+            self.botones[texto_boton].setFixedSize(40,40)
+            # Publicamos el boton en el grid layout
+            layout_botones.addWidget(self.botones[texto_boton], posicion[0],posicion[1])
+        # Agregamos el layout de botones al layout principal
         self.layout_principal.addLayout(layout_botones)
 
-    def conectar_botones(self):
-        for text_boton, boton in self.botones.items():
-            if text_boton not  in {'=','C'}:
-                #boton.clicked.connect(lambda: self._construir_exprecion(text_boton))
-                boton.clicked.connect(partial(self._construir_exprecion, text_boton))
+    def _conectar_botones(self):
+        # Recorrer cada boton del diccionario (key:value) (texto:PushButton)
+        for texto_boton, boton in self.botones.items():
+            if texto_boton not in {'=','C'}:
+                # boton.clicked.connect(lambda: self._construir_expresion(texto_boton))
+                boton.clicked.connect(partial(self._construir_expresion, texto_boton))
 
+    def _construir_expresion(self, texto_boton):
+        expresion = self.obtener_texto() + texto_boton
+        # Actualizamos la linea de entrada
+        self.actualizar_texto(expresion)
 
+    def obtener_texto(self):
+        return self.linea_entrada.text()
 
+    def actualizar_texto(self, texto):
+        self.linea_entrada.setText(texto)
+        self.linea_entrada.setFocus()
 
-
-
-
-
-
+    def _limpiar_linea_entrada(self):
+        self.actualizar_texto('')
 
 if __name__ == '__main__':
     app = QApplication()
